aeadf481139476c6fad95442adaf1515151aea3c
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-02-25 17:03:23 +0100

    Add(Data)

---
+++ Code/src/db/mock-author.mjs
@@ -0,0 +1,40 @@
+const auteurs = [
+    {
+      auteur_id: 1,
+      nom: "Théodore",
+      prenom : "Kaczynski",
+    },
+    {
+      auteur_id: 2,
+      nom: "Orwell",
+      prenom : "George",
+    },
+    {
+      auteur_id: 3,
+      nom: "Huxley",
+      prenom : "Aldous",
+    },
+    {
+      auteur_id: 4,
+      nom: "Aurèle",
+      prenom : "Marc",
+    },
+    {
+      auteur_id: 5,
+      nom: "FortniteBG",
+      prenom : "Publishing",
+    },
+    {
+      auteur_id: 6,
+      nom: "Miura",
+      prenom : "Kentaro",
+    },
+    {
+      auteur_id: 7,
+      nom: "Ellison",
+      prenom : "Harlan",
+    },
+  ];
+  
+  export default auteurs;
+  
