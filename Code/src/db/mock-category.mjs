62c1ff2c0e4db4727bebeb452d28497d251594d1
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-02-25 17:13:37 +0100

    Create(Mockups)
    - Now have more data :)

---
+++ Code/src/db/mock-category.mjs
@@ -0,0 +1,24 @@
+const categories = [
+    {
+      categorie_id: 1,
+      libelle: "Philosophie",
+    },
+    {
+      categorie_id: 2,
+      libelle: "Politique",
+    },
+    {
+      auteur_id: 3,
+      libelle: "Science-fiction",
+    },
+    {
+      auteur_id: 4,
+      libelle: "Dessin",
+    },
+    {
+      auteur_id: 5,
+      libelle: "Dark fantasy",
+    },
+  ];
+  
+  export default categories;
---
+++ Code/src/db/mock-editor.mjs
@@ -0,0 +1,16 @@
+const editeurs = [
+    {
+      editeur_id: 1,
+      nom: "Pocket",
+    },
+    {
+      editeur_id: 2,
+      nom: "FortniteBG Publishing",
+    },
+    {
+      editeur_id: 3,
+      nom: "Hakusensha",
+    },
+  ];
+  
+  export default editeurs;
