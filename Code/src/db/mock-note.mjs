4341972e623e37ef7ebdc19637a350e9c31ec59b
ThomasDelETML <po51oro@eduvaud.ch>
2025-03-04 20:49:00 +0100

    Update (mock Notes) : mock notes fini

--- Code/src/db/mock-note.mjs
+++ Code/src/db/mock-note.mjs
@@ -49,6 +49,131 @@ const commentaires = [
       utilisateur_id: 5, // TheVoicesInMyHead
       note: 6,
       },
+    {///Livre3
+      livre_id: 3,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 9,
+    },
+    {
+      livre_id: 3,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 5,
+    },
+    {
+      livre_id: 3,
+      utilisateur_id: 3, // Captain_Peru
+      note: 7,
+    },
+    {
+      livre_id: 3,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 8,
+    },
+    {
+      livre_id: 3,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 5,
+      },
+      {///Livre4
+        livre_id: 4,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 6,
+    },
+    {
+      livre_id: 4,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 8,
+    },
+    {
+      livre_id: 4,
+      utilisateur_id: 3, // Captain_Peru
+      note: 4,
+    },
+    {
+      livre_id: 4,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 10,
+    },
+    {
+      livre_id: 4,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 7,
+      },
+    {///Livre5
+      livre_id: 5,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 6,
+    },
+    {
+      livre_id: 5,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 4,
+    },
+    {
+      livre_id: 5,
+      utilisateur_id: 3, // Captain_Peru
+      note: 2,
+    },
+    {
+      livre_id: 5,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 10,
+    },
+    {
+      livre_id: 5,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 4,
+    },
+    {///Livre6
+      livre_id: 6,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 9,
+    },
+    {
+      livre_id: 6,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 9,
+    },
+    {
+      livre_id: 6,
+      utilisateur_id: 3, // Captain_Peru
+      note: 9,
+    },
+    {
+      livre_id: 6,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 10,
+    },
+    {
+      livre_id: 6,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 10,
+    },
+    {///Livre7
+      livre_id: 7,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 9,
+    },
+    {
+      livre_id: 7,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 7,
+    },
+    {
+      livre_id: 7,
+      utilisateur_id: 3, // Captain_Peru
+      note: 5,
+    },
+    {
+      livre_id: 7,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 5,
+    },
+    {
+      livre_id: 7,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 10,
+      },
   ];
   
   export default commentaires;
