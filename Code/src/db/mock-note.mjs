9315e84a7971c26282ec173b7ced9a22e7f8baf4
Romain <163823947+romaindenis1@users.noreply.github.com>
2025-03-04 17:18:35 +0100

    Update mock-note.mjs

---
+++ Code/src/db/mock-note.mjs
@@ -0,0 +1,55 @@
+const commentaires = [
+    {
+      livre_id: 1,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 10,
+    },
+    {
+      livre_id: 1,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 10,
+    },
+    {
+      livre_id: 1,
+      utilisateur_id: 3, // Captain_Peru
+      note: 9,
+    },
+    {
+      livre_id: 1,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 5,
+    },
+    {
+      livre_id: 1,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 6,
+    },
+    {///Livre2
+        livre_id: 2,
+      utilisateur_id: 1, // Theodore Kaczynski
+      note: 9,
+    },
+    {
+      livre_id: 2,
+      utilisateur_id: 2, // UrLocalBartender
+      note: 2,
+    },
+    {
+      livre_id: 2,
+      utilisateur_id: 3, // Captain_Peru
+      note: 7,
+    },
+    {
+      livre_id: 2,
+      utilisateur_id:4, // SuperMegaUltraGigaChad
+      note: 4,
+    },
+    {
+      livre_id: 2,
+      utilisateur_id: 5, // TheVoicesInMyHead
+      note: 6,
+      },
+  ];
+  
+  export default commentaires;
+  
