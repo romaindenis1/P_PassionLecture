63942f57098355f445873ad93c976baf8383a85c
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-03-11 16:24:32 +0100

    perf(mock-editor.mjs) -> Correction des syntaxes + commentaires

--- Code/src/db/mock-editor.mjs
+++ Code/src/db/mock-editor.mjs
@@ -1,16 +1,9 @@
+// Liste des éditeurs avec identifiant et nom
 const editeurs = [
-    {
-      editeur_id: 1,
-      nom: "Pocket",
-    },
-    {
-      editeur_id: 2,
-      nom: "FortniteBG Publishing",
-    },
-    {
-      editeur_id: 3,
-      nom: "Hakusensha",
-    },
-  ];
-  
-  export default editeurs;
+  { editeur_id: 1, nom: "Pocket" },
+  { editeur_id: 2, nom: "FortniteBG Publishing" },
+  { editeur_id: 3, nom: "Hakusensha" },
+];
+
+// Export pour pouvoir utiliser la liste dans d'autres fichiers
+export default editeurs;
