7ab3d3b7b705d96980a2b97cdda47eefce0aefd4
GHerreraE <163824038+GHerreraE@users.noreply.github.com>
2025-03-11 16:28:03 +0100

    feat(uploadMiddleware.mjs) -> Création du middleware pour gérer les images

---
+++ Code/src/middleware/uploadMiddleware.mjs
@@ -0,0 +1,34 @@
+import multer from "multer"; // Pour gérer les uploads
+import path from "path"; // Pour gérer les chemins de fichiers
+
+// Config de stockage pour multer
+const storage = multer.diskStorage({
+  // Dossier où les fichiers seront enregistrés
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  // Nom du fichier: timestamp + extension originale
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + path.extname(file.originalname));
+  },
+});
+
+// Middleware d'upload avec configuration et restrictions
+const upload = multer({
+  storage: storage,
+  limits: { fileSize: 1 * 3840 * 2160 }, // Limite de taille en octets
+  fileFilter: (req, file, cb) => {
+    const fileTypes = /jpeg|jpg|png/; // Types de fichiers acceptés
+    const extname = fileTypes.test(
+      path.extname(file.originalname).toLowerCase()
+    );
+    const mimeType = fileTypes.test(file.mimetype);
+    if (extname && mimeType) {
+      return cb(null, true);
+    }
+    cb(new Error("Seulement les fichiers JPEG, JPG, ou PNG sont autorisés"));
+  },
+});
+
+// Export du middleware upload
+export { upload };
