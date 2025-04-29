f0cc8c9923a2b383bdfc95f4055a42b5902d59ec
Gonzalo Javier Herrera Egoavil <py78dal@eduvaud.ch>
2025-04-29 15:51:15 +0200

    chore(frontend): remake projet

--- src/Frontend/passion-lecture/src/router/index.js
+++ src/Frontend/passion-lecture/src/router/index.js
@@ -1,21 +1,22 @@
 import { createRouter, createWebHistory } from 'vue-router'
-import BookListView from '../views/BookListView.vue'
-//import BookDetailsView from '../views/BookDetailsView.vue'
+import HomeView from '../views/HomeView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
     {
       path: '/',
-      name: 'book-list',
-      component: BookListView,
+      name: 'home',
+      component: HomeView,
     },
- /*   {
-      path: '/book/:id',
-      name: 'book-details',
-      props: true,
-      component: BookDetailsView,
-    },*/
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: () => import('../views/AboutView.vue'),
+    },
   ],
 })
 
