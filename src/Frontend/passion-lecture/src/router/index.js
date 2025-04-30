7378273c3749732884a3e6f893a9eea6ec371d7b
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-30 08:16:14 +0200

    Update index.js
    - Added a route for category filter

--- src/Frontend/passion-lecture/src/router/index.js
+++ src/Frontend/passion-lecture/src/router/index.js
@@ -1,6 +1,7 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import ConnectionBackend from '@/components/connectionBackend.vue'
 import AccueilView from '@/views/AccueilView.vue'
+import categoryFilter from '@/components/categoryFilter.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -10,6 +11,11 @@ const router = createRouter({
       path: '/livres',
       name: 'accueil',
       component: AccueilView,
+    },
+    {
+      path: '/categories/:id/livres',
+      name: 'CategoryBooks',
+      component: categoryFilter,
     },
   ],
 })
