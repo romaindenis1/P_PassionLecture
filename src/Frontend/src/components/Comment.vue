5f5d6680a65c1e2f7dbd9f830eed7fc11c3911fa
Romain Pierre Marie Denis <ps04egl@eduvaud.ch>
2025-04-30 11:03:40 +0200

    feat(Comment Display)
    - Now displays comments under a book

---
+++ src/Frontend/src/components/Comment.vue
@@ -0,0 +1,12 @@
+<script setup>
+defineProps({
+  comment: Object,
+})
+</script>
+
+<template>
+  <div>
+      <h3 style="font-weight: bold;">{{ comment.t_utilisateur.username }}</h3>
+      <p>{{ comment.contenu }}</p>
+  </div>
+</template>
